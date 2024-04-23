06124e8699526b19a123495be3fc20ab6767fbeb
Francisco Ciordia Cantarella <gatupancho@gmail.com>
2024-04-23 00:11:41 +0000

    hasta 5

--- Homework/Homework04/Homework04.hs
+++ Homework/Homework04/Homework04.hs
@@ -1,3 +1,5 @@
+import Control.Applicative (Alternative(empty))
+import Data.Binary.Get (Decoder(Fail))
 -- Question 1
 -- Lets say you have the nested values defined bellow. How would you get the value of
 -- 4 by using only pattern matching in a function?
@@ -5,26 +7,47 @@
 nested :: [([Int], [Int])]
 nested = [([1,2],[3,4]), ([5,6],[7,8])]
 
+saca4 :: [([Int], [Int])] -> Int
+saca4 [(_,[_,a]),_] = a
+saca4 _ = error "No esta"
+
 -- Question 2
 -- Write a function that takes a list of elements of any type and, if the list has 3 or more elements, it
 -- removes them. Else, it does nothing. Do it two times, one with multiple function definitions and one with
 -- case expressions.
+remover3 :: [a] -> [a]
+remover3 (_:_:_:rest) = rest
+remover3 x = x
 
+remover3' :: [a] -> [a]
+remover3' x = case x of
+    (_:_:_:rest) -> rest
+    x -> x
 
 -- Question 3
 -- Create a function that takes a 3-element tuple (all of type Integer) and adds them together
+sum3 :: (Int,Int,Int) -> Int
+sum3 (a,b,c) = a+b+c
+
 
 
 -- Question 4
 -- Implement a function that returns True if a list is empty and False otherwise.
-
+isVacia :: [a] -> Bool
+isVacia [] = True
+isVacia _ = False
 
 -- Question 5
 -- Write the implementation of the tail function using pattern matching. But, instead of failing if
 -- the list is empty, return an empty list.
-
+myTail :: [a] -> [a]
+myTail (_:xs) = xs
+myTail [] = []
 
 -- Question 6
 -- write a case expression wrapped in a function that takes an Int and adds one if it's even. Otherwise does nothing. 
 -- (Use the `even` function to check if the number is even.)
-
+addEven :: Int -> Int
+addEven x = case even x of
+    True -> x+1
+    False -> x
--- Homework/Homework05/Homework05.hs
+++ Homework/Homework05/Homework05.hs
@@ -1,26 +1,43 @@
 -- Create a higher-order function that takes 3 parameters: A function and the two parameters that that function takes, and
 -- flips the order of the parameters.
 -- For example this: `(/) 6 2` returns `3`. But this: `flip' (/) 6 2` returns `0.3333333333`
-
+funcFlip :: (a -> a -> a) -> a -> a -> a
+funcFlip f x y = f y x
 
 -- Create the `uncurry'` function that converts a curried function to a function on pairs. So this: `(+) 1 2` that returns `3` can be written as
 -- `uncurry' (+) (1,2)` (with the two different arguments inside a pair).
-
+funcUnCurry :: (a -> b -> c) -> (a,b) -> c
+funcUnCurry func (x,y) = func x y
 
 -- Create the `curry'` function that converts an uncurried function to a curried function. So this: `fst (1,2)` that returns `1` can be written as
 -- `curry' fst 1 2` (with the tuple converted into two different arguments).
+funcCurry :: ((a,b) -> c) -> a -> b -> c
+funcCurry func x y = func (x,y)
 
 
 -- Use higher-order functions, partial application, and point-free style to create a function that checks if a word has an uppercase letter.
 -- Start with using just higher-order functions and build from there. 
+isCharUpper :: Char -> Bool
+isCharUpper c = c >= 'A' && c <= 'Z'
 
+isUpper' :: (Char -> Bool) -> String -> Bool
+isUpper' = any
+
+isUpperRedy :: String -> Bool
+isUpperRedy = isUpper' isCharUpper
 
 -- Create the `count` function that takes a team ("Red", "Blue", or "Green") and returns the amount of votes the team has inside `votes`.
 
 votes :: [String]
 votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
 
+count' :: String -> Int
+count' t = length $ filter (==t) votes
+
 -- Create a one-line function that filters `cars` by brand and then checks if there are any left.
 
 cars :: [(String,Int)]
 cars = [("Toyota",0), ("Nissan",3), ("Ford",1)]
+
+anyCarsLeft :: String -> Bool
+anyCarsLeft brand = any (\(b, num) -> b == brand && num > 0) cars
