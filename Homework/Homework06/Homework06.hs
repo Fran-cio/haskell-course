ce5693d335d07fe99582cb1df532a6c39644b6af
Francisco Ciordia Cantarella <gatupancho@gmail.com>
2024-04-23 14:48:31 +0000

    Hasta el 6++

--- Homework/Homework06/Homework06.hs
+++ Homework/Homework06/Homework06.hs
@@ -4,6 +4,9 @@
 --
 -- >>> repeat 17
 --[17,17,17,17,17,17,17,17,17...
+repeat' :: a -> [a]
+-- repeat' n || n => 0 = []
+repeat' n = n:repeat' n
 
 
 -- Question 2
@@ -17,14 +20,18 @@
 -- []
 -- >>> replicate 4 True
 -- [True,True,True,True]
-
+replicate' :: Int -> a -> [a]
+replicate' n _ | n <= 0 = []
+replicate' n a = take n (repeat' a)
 
 -- Question 3
 -- Write a function called `concat'` that concatenates a list of lists.
 --
 -- >>> concat' [[1,2],[3],[4,5,6]]
 -- [1,2,3,4,5,6]
-
+concat' :: [[a]] -> [a]
+concat' [] = []
+concat' (l:ls) = l ++ concat' ls
 
 -- Question 4
 -- Write a function called `zip'` that takes two lists and returns a list of
@@ -44,6 +51,10 @@
 -- []
 -- >>> zip' [1..] []
 -- []
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (a:as) (b:bs) = (a,b) : zip' as bs
 
 
 
@@ -60,6 +71,10 @@
 -- >>> zipWith (+) [1, 2, 3] [4, 5, 6]
 -- [5,7,9]
 
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' func (a:as) (b:bs) = func a b : zipWith' func as bs
 
 -- Question 6
 -- Write a function called `takeWhile'` that takes a precate and a list and
@@ -72,11 +87,35 @@
 -- >>> takeWhile (< 0) [1,2,3]
 -- []
 
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' _ [] = []
+takeWhile' bf (x:xs)  
+  | bf x = x : takeWhile' bf xs
+  | otherwise = []
 
 -- Question 7 (More difficult)
 -- Write a function that takes in an integer n, calculates the factorial n! and
 -- returns a string in the form of 1*2* ... *n = n! where n! is the actual result.
 
+
+factS :: Int -> String
+factS n = let
+    fact 0 = 1
+    fact n = n * fact (n-1)
+    factString n 1 = show n ++ " = " ++ show (fact n)
+    factString n acum = show (n - (acum - 1)) ++ " * " ++ factString n (acum - 1)
+  in
+    factString n n
+
+secuenciaFib :: Int -> [Int]
+secuenciaFib n = let
+    fib 1 = 1
+    fib 0 = 0
+    fib n = fib (n-2) + fib (n-1)
+    secFib 0 = [0]
+    secFib n = secFib (n-1) ++ [fib n]
+  in
+    secFib n
 
 -- Question 8
 -- Below you have defined some beer prices in bevogBeerPrices and your order list in
@@ -100,3 +139,11 @@ orderList =
 
 deliveryCost :: Double
 deliveryCost = 8.50
+
+totalCost :: Double
+totalCost = let
+    parcialCost _ [] = 0
+    parcialCost [] _ = error "No tengo esa birra"
+    parcialCost ((_,p):ps) ((_,o):os) = o * p + parcialCost ps os
+  in
+    parcialCost bevogBeerPrices orderList + deliveryCost
